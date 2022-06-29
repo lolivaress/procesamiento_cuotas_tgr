66d3c618faa0b517cac9ec36751ed63f268b1e05
leonardo <lolivaress@gmail.com>
2022-06-28 22:01:30 -0400

    v3

--- main.py
+++ main.py
@@ -16,17 +16,9 @@ for file in files:
            procesa_tipos.procesa_fallecidos(file,cuota)
         else:
            procesa_tipos.procesa_tipo_1(file,cuota)
-           #procesa_tipos.procesa_tipo_2(file,cuota)
-           #procesa_tipos.procesa_tipo_3(file,cuota)
+           procesa_tipos.procesa_tipo_2(file,cuota)
+           procesa_tipos.procesa_tipo_3(file,cuota)
 
-
-
-cnn = db.SQLAlchemyConnection('localhost','dgio_procesa_tgr_2022')
-cnn.connect()
-tabla = f"cuota_{cuota}_T2"
-query_string = f"exec dbo.sp_normaliza_sw {tabla}"
-cnn.execute(query_string)
-cnn.close()
 
         
         
